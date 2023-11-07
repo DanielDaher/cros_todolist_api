5734e26d9c9209e00df36ed3e91466e9e1b37d35
Daniel <danielfdoficial@gmail.com>
2023-11-07 18:18:16 -0300

    refactor: organize app and remove old files

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,20 @@
 import express from 'express';
-import morgan from 'morgan';
-import helmet from 'helmet';
 import cors from 'cors';
+import usersRoute from './Routes/usersRoute';
+import tasksRoute from './Routes/tasksRoute';
+import loginRoute from './Routes/loginRoute';
+
 const app = express();
-
-
 
 app.use(express.json({
   type: ['application/json', 'text/plain']
 })); //https://stackoverflow.com/questions/54016068/empty-body-in-fetch-post-request
 
 app.use(cors());
+
+app.use('/tasks', tasksRoute.router);
+app.use('/login', loginRoute.router);
+app.use('/users', usersRoute.router);
 
 
 
