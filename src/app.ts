58f59cb150acbe9d2cc9c966b163b3e262a91164
Daniel <danielfdoficial@gmail.com>
2023-11-08 12:47:43 -0300

    feat: swagger documentation

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,24 @@
-import express from 'express';
-import cors from 'cors';
-import usersRoute from './Routes/usersRoute';
-import tasksRoute from './Routes/tasksRoute';
-import loginRoute from './Routes/loginRoute';
+import express from "express";
+import cors from "cors";
+import usersRoute from "./Routes/usersRoute";
+import tasksRoute from "./Routes/tasksRoute";
+import loginRoute from "./Routes/loginRoute";
+import swaggerUi from "swagger-ui-express";
+import * as swaggerDocument from "./swagger.json";
 
 const app = express();
 
-app.use(express.json({
-  type: ['application/json', 'text/plain']
-})); //https://stackoverflow.com/questions/54016068/empty-body-in-fetch-post-request
+app.use(
+  express.json({
+    type: ["application/json", "text/plain"]
+  })
+); //https://stackoverflow.com/questions/54016068/empty-body-in-fetch-post-request
 
 app.use(cors());
 
-app.use('/tasks', tasksRoute.router);
-app.use('/login', loginRoute.router);
-app.use('/users', usersRoute.router);
-
-
-
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/tasks", tasksRoute.router);
+app.use("/login", loginRoute.router);
+app.use("/users", usersRoute.router);
 
 export default app;
